d0191da9cea589f16f77c59b12de308d3cc3a079
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-09-13 19:53:41 -0400

    set up of initial preliminary folder structure.

---
+++ Models/index.js
@@ -0,0 +1,7 @@
+// SAMPLE DATA  -  However this is redundant
+
+const User = require('./User');
+const Tag = require('./Tag');
+const Application = require('./Application');
+
+module.exports = { User, Application, Tag };
---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+// SAMPLE DATA
+
+const router = require('express').Router();
+const appRoutes = require('./appRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/apps', appRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
